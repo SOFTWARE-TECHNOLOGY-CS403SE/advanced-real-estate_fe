a652ec3bf34d0a564bd2835601948bbc3dd89911
Clearlove7 <88828156+tuongclearlove7@users.noreply.github.com>
2024-10-09 13:14:20 +0700

    refactor: set auth token

--- advanced-real-estate-app/src/App.js
+++ advanced-real-estate-app/src/App.js
@@ -7,9 +7,15 @@ function App() {
     const [services, setServices] = useState([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
+    //add token
+    const token = "eyJhbGciOiJIUzUxMiJ9.eyJpc3MiOiJleGFtcGxlLm9yZyIsInN1YiI6ImFkbWluIiwiZXhwIjoxNzI4NDU3NzQ3LCJpYXQiOjE3Mjg0NTQxNDcsImp0aSI6IjZmYWJhN2MzLWY3OWEtNGVjMi05ZjNjLTllNmNjYmIyZjkzYyIsInNjb3BlIjoiUk9MRV9BRE1JTiJ9.2AbYmkxm7Sn5rWmexylB33q-V9ef0uqvXT8lmCvn51prSstqb7XgFYYDuh6NG7lw9vAuXiPM2ULl1ZWF0sfsFg";
 
     useEffect(() => {
-        axios.get('http://localhost:8080/api/user/services')
+        axios.get('http://localhost:8080/api/user/services', {
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
+        })
             .then((response) => {
                 setServices(response.data);
                 console.log(response.data)
